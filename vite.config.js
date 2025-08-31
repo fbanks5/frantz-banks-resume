f15a93aa7b841888afc34e7250834d6ad79b5f7e
Frantz Banks III <frantz7101990@gmail.com>
2025-08-31 10:37:53 -0400

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react'
 import path from "path"
 
 export default defineConfig({
-    plugins: [react( )],
+    plugins: [react()],
     base: '/frantz-banks-resume/',
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets'
+    },
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
